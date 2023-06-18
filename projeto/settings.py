f569116841d2bb6f4355cb88640a74af1559e4dd
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-18 12:21:15 -0300

    Static ROOT

--- projeto/settings.py
+++ projeto/settings.py
@@ -79,9 +79,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'statics')
 ]
 
 MEDIA_URL = 'media/'
