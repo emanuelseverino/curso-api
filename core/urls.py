d604f4087142d29d041b704961fe85c04fada45e
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-18 15:54:24 -0300

    Admin e Painel

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import IndexView
+from core.views import IndexView, PainelView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('painel/', PainelView.as_view(), name='painel'),
 ]
