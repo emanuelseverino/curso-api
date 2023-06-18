b8fb8bc87299b7f85977eb13f8e5f30031d42d80
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-18 15:14:54 -0300

    REfatorando o design

--- projeto/settings.py
+++ projeto/settings.py
@@ -91,7 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuario.CustomUsuario'
 
-LOGIN_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = 'login'
 
