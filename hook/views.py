271b83ea381832f0c8f1282d0224287ee0ce6354
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-19 20:53:26 -0300

    Pagamento3

--- hook/views.py
+++ hook/views.py
@@ -96,7 +96,7 @@ class PagarView(LoginRequiredMixin, View):
 class WebHookView(View):
 
     def post(self, request, *args, **kwargs):
-        teste = Teste(mensagem=str(self))
+        teste = Teste(mensagem=str(self.request))
         teste.save()
 
         mercado_page_test = MercadoPago(
