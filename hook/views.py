37ed9f7d3e6a9d629356f0228fa78d5d2d52c4e2
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-19 18:34:10 -0300

    Pagamento3

--- hook/views.py
+++ hook/views.py
@@ -19,7 +19,7 @@ class PagarView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         data = {
-            "transaction_amount": 101,
+            "transaction_amount": 3,
             "description": "Compra API2",
             "payment_method_id": "pix",
             "payer": {
